9cef1c8a2eca659cbc8f3cce9286564351dfcd0d
Gabriel Pettier <gabriel.pettier@gmail.com>
2010-01-09 22:32:56 +0100

    added interpreter and simplified make_deb.sh

--- make_deb.sh
+++ make_deb.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env sh
+# This script is part of ultimate-smash-friends
+# this script build a debian/ubuntu package for ultimate-smash-friends
+
 mkdir tmp/
-cd tmp/
-unzip ../doc/ultimate-smash-friends.zip
-cd ..
+unzip doc/ultimate-smash-friends.zip -d tmp
 cp code tmp/ultimate-smash-friends/usr/share/games/ -R
 mv tmp/ultimate-smash-friends/usr/share/games/code tmp/ultimate-smash-friends/usr/share/games/ultimate-smash-friends
-cd tmp
-dpkg -b ultimate-smash-friends/ ../ultimate-smash-friends.deb
-cd ..
+dpkg -b tmp/ultimate-smash-friends/ ultimate-smash-friends.deb
 rm tmp/ -R
