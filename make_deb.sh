11b6388a78b6e898ca1270bde89dbb5c50730ae2
gaby <gaby@ks22672.kimsufi.com>
2010-01-15 02:03:15 +0100

    Just delete mp3 in de creation.

--- make_deb.sh
+++ make_deb.sh
@@ -5,6 +5,8 @@
 mkdir tmp/
 unzip doc/ultimate-smash-friends.zip -d tmp
 cp code tmp/ultimate-smash-friends/usr/share/games/ -R
+rm tmp/ultimate-smash-friends/usr/share/games/usf_modules/
+rm -r tmp/ultimate-smash-friends/usr/share/games/code/usf_media/musics/mp3/
 mv tmp/ultimate-smash-friends/usr/share/games/code tmp/ultimate-smash-friends/usr/share/games/ultimate-smash-friends
 dpkg -b tmp/ultimate-smash-friends/ ultimate-smash-friends.deb
 rm tmp/ -R
