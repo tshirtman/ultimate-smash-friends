6f8ad0a16c6cce75dca052337579bb7d7992924e
xapantu <xapantu@gmail.com>
2010-04-24 20:01:35 +0200

    Split new_gui.py in 2 files : new_gui and skin, removing items which will be rewriten

--- usf/skin.py
+++ usf/skin.py
@@ -33,55 +33,38 @@ from config import Config
 config = Config.getInstance()
 
 import controls
-import entity_skin
-from game import Game
-import controls
 import loaders
-# Gui modules
-from new_widgets import (Widget, WidgetLabel)
-from skin import Skin
+
 #translation
 import translation
 
-
-class NewGui(object):
-    """
-    Main class of the GUI. Init and maintain all menus and widgets.
-
-    """
-    def __init__(self, screen):
-        self.screen = screen
-    def update(self, first, second, third):
-        pass
-        return False,None
-skin = Skin()
-class Dialog(object):
-    state = False
-    image = None
-    def __init__(self, screen, name):
-        global skin
-        self.background = loaders.image(
-            config.sys_data_dir+
+class Skin (object):
+    dialog = {}
+    color = None
+    background = []
+    background_duration = []
+    def __init__(self):
+        xml_file = xml.dom.minidom.parse(config.sys_data_dir+
             os.sep+
             'gui'+
-            os.sep+
-            config.general['THEME']+
-            os.sep+
-            "background-dialog.png", scale=(skin.dialog['sizex'], skin.dialog['sizey'])
-            )[0]
-        self.background.set_alpha(150)
-        self.screen = screen
-    def draw(self):
-        self.screen.blit(self.tmp_screen, (0,0))
-        self.screen.blit(self.background, (skin.dialog['posx'], skin.dialog['posy']))
-    def show(self):
-        if self.state is False:
-            self.state = True
-            self.tmp_screen = self.screen.copy()
-            cache = pygame.Surface((config.general['WIDTH'], config.general['HEIGHT']))
-            cache.fill(pygame.color.Color("black"))
-            cache.set_alpha(100)
-            self.tmp_screen.blit(cache, (0,0))
-        else:
-            self.state = False
-            self.tmp_screen = None
+            os.sep+ config.general['THEME'] + os.sep + "theme.xml").getElementsByTagName("theme")[0]
+        self.color = pygame.color.Color("white")
+        for node in xml_file.childNodes:
+            try:
+                if node.tagName == "color":
+                    self.color = pygame.color.Color(str(node.getAttribute("value")))
+                elif node.tagName == "dialog":
+                    self.dialog['sizex'] = int(node.getAttribute("sizex"))*config.general['WIDTH']/100
+                    self.dialog['sizey'] = int(node.getAttribute("sizey"))*config.general['HEIGHT']/100
+                    self.dialog['posx'] = int(node.getAttribute("posx"))*config.general['WIDTH']/100
+                    self.dialog['posy'] = int(node.getAttribute("posy"))*config.general['HEIGHT']/100
+                elif node.tagName == "background":
+                    for child_node in node.childNodes:
+                        try:
+                            if child_node.tagName == "img":
+                                self.background.append(child_node.getAttribute("src"))
+                                self.background_duration.append(float(child_node.getAttribute("time")))
+                        except:
+                            pass
+            except:
+                pass
