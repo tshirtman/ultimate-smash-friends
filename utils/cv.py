9710a0352026ede6c24b5cf6ff84659b78e56e05
Gabriel Pettier <gabriel.pettier@gmail.com>
2010-08-15 05:34:04 +0200

    added a xml viewer to quickly see changes while editing an xml file some correction on possum xml

---
+++ utils/cv.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import pygame
+from pygame import locals
+from pygame.locals import (
+    KEYDOWN,
+    KEYUP,
+    MOUSEMOTION,
+    MOUSEBUTTONUP,
+    MOUSEBUTTONDOWN,
+    USEREVENT,
+    K_ESCAPE,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_SPACE,
+    K_F5,
+    )
+
+import sys, os
+import time
+
+usf_root='../'
+
+sys.path.append(os.path.join(usf_root,'usf'))
+
+from entity_skin import Entity_skin
+from loaders import image
+
+def load(charname):
+    return Entity_skin(os.path.join(
+        usf_root, 'data', 'characters', charname
+        ))
+
+def main(charname):
+
+    pygame.init()
+    screen = pygame.display.set_mode((200,200))
+    entity_skin = load(charname)
+    anim = 0
+
+    bottom_center_hardshape = (100, 150)
+    while True:
+        # get key events
+        pygame.event.pump()
+        for event in pygame.event.get(
+            [ KEYDOWN, KEYUP ]
+            ):
+            if event.key == K_ESCAPE:
+                return
+            elif event.key == K_F5:
+                character = load(charname)
+            elif event.key == K_UP:
+                animation +=1
+            elif event.key == K_DOWN:
+                animation -=1
+
+        animation = entity_skin.animations.keys()[
+            anim % len(entity_skin.animations.keys())
+        ]
+        pygame.event.clear( [MOUSEMOTION, MOUSEBUTTONUP, MOUSEBUTTONDOWN] )
+        # update screen
+        screen.fill(pygame.Color('green'))
+        img = filter(
+                    lambda f : f.time < (
+                        (time.time()*1000) %
+                        entity_skin.animations[animation].duration
+                        ),
+                    entity_skin.animations[animation].frames
+                    )[-1]
+        print img
+        position = (
+            bottom_center_hardshape[0] - img.hardshape[0] - img.hardshape[2]/2,
+            bottom_center_hardshape[1] - img.hardshape[1] - img.hardshape[3]/2
+        )
+        screen.blit(image(img.image)[0], position)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main(sys.argv[1])
