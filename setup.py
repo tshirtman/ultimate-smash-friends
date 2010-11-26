b507f4519e987feb3deae54c512ed9a76039398e
edwin <edwin@enigma>
2010-11-26 11:14:16 -0600

    Simplified setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import with_statement
 
 """ The files method was adapted from the AutoDataDiscovery solution in the
     official Distutils Cookbook:
@@ -42,101 +41,70 @@ def isSystemDLL(pathname):
         return 0
     return origIsSystemDLL(pathname)
 
-if OS == 'windows':
-    origIsSystemDLL = py2exe.build_exe.isSystemDLL
-    py2exe.build_exe.isSystemDLL = isSystemDLL
-    
-    DATA = [(item[0], item[1]) for item in files('data')]
-    DATA.append('CREDITS.txt')
-elif OS == 'darwin':
-    DATA = [(item[0], item[1]) for item in files('data')]
-    DATA.append('CREDITS.txt')
-else:
-    DATA = [(join('share', 'ultimate-smash-friends') + sep + item[0], item[1])
-            for item in files('data')]
-    DATA.append((join('share', 'ultimate-smash-friends') + sep + 'data', ['CREDITS.txt']))
 
 NAME = 'ultimate-smash-friends'
 VERSION = '0.1.3'
-
+DESCRIPTION = ('A 2d arcade fight game, based on the gameplay of super '
+                'smash bros, from nintendo.')
+AUTHOR = 'Gabriel Pettier'
+AUTHOR_EMAIL = 'gabriel.pettier@gmail.com'
+MAINTAINER = 'Lucas Baudin (xapantu)'
+MAINTAINER_EMAIL = 'xapantu@gmail.com'
+URL = 'http://usf.tuxfamily.org/'
+CLASSIFIERS = ['Development Status :: 2 - Pre-Alpha',
+               'Operating System :: OS Independent',
+               'Intended Audience :: End Users/Desktop',
+               'License :: OSI Approved :: GNU General Public License (GPL)',
+               'Natural Language :: English',
+               'Programming Language :: Python',
+               'Topic :: Games/Entertainment :: Arcade'
+              ]
 DOC = [(join('share', 'doc', 'ultimate-smash-friends') +
        item[0].replace('doc', ''), item[1]) for item in files('doc')]
 DOC[-1][-1].append('COPYING.txt')
 DOC[-1][-1].append('CREDITS.txt')
 DOC[-1][-1].append('README.txt')
 DOC[-1][-1].append('README.fr.txt')
-
 CONFIG = [(sep + join('etc', 'ultimate-smash-friends'), ['system.cfg'])]
-
 ICON = [(join('share', 'applications'), 
               ['ultimate-smash-friends.desktop'])]
-
 SCRIPTS = ['ultimate-smash-friends',
            'viewer.pyw', 'utils/togimpmap.py', 
            'utils/tolevel.py', 
-           'utils/xml_text_extractor.py']
+           'utils/xml_text_extractor.py'
+          ]
+PACKAGES = ['usf', 'usf.widgets', 'usf.screen', 'usf.subpixel']
+REQUIRES = ['pygame (>=1.6)', 'python (>=2.5)']
 
-if OS == 'darwin':
+if OS == 'windows':
+    origIsSystemDLL = py2exe.build_exe.isSystemDLL
+    py2exe.build_exe.isSystemDLL = isSystemDLL
+    DATA = [(item[0], item[1]) for item in files('data')]
+    DATA.append('CREDITS.txt')
+    WINDOWS=[{"script" : "ultimate-smash-friends", "icon_resources" : [(1, "data/icon/icon.ico")]}]
+
+elif OS == 'darwin':
+    DATA = [(item[0], item[1]) for item in files('data')]
+    DATA.append('CREDITS.txt')
     SCRIPTS[0] = 'ultimate-smash-friends.py'
 
-    PLIST = dict(
-        CFBundleName=NAME,
-        CFBundleShortVersionString=VERSION,
-        CFBundleGetInfoString=' '.join([NAME, VERSION]),
-        CFBundleExecutable=NAME,
-        CFBundleIdentifier='org.pythonmac.ultimate-smash-friends'
-    )
-
+    PLIST = dict(CFBundleName=NAME,
+                 CFBundleShortVersionString=VERSION,
+                 CFBundleGetInfoString=' '.join([NAME, VERSION]),
+                 CFBundleExecutable=NAME,
+                 CFBundleIdentifier='org.pythonmac.ultimate-smash-friends'
+                )
+    app=[dict(script='ultimate-smash-friends.py', plist=PLIST)]
     OPTIONS = {'argv_emulation': True}
-
-    setup(name=NAME,
-          version=VERSION,
-          description=('A 2d arcade fight game, based on the gameplay of super '
-                       'smash bros, from nintendo.'),
-          author='Gabriel Pettier',
-          author_email='gabriel.pettier@gmail.com',
-          maintainer='Lucas Baudin (xapantu)',
-          maintainer_email='xapantu@gmail.com',
-          url='http://usf.tuxfamily.org/',
-          classifiers=['Development Status :: 2 - Pre-Alpha',
-                       'Operating System :: OS Independent',
-                       'Intended Audience :: End Users/Desktop',
-                       'License :: OSI Approved :: GNU General Public License (GPL)',
-                       'Natural Language :: English',
-                       'Programming Language :: Python',
-                       'Topic :: Games/Entertainment :: Arcade'
-                      ],
-          app=[
-            dict(script='ultimate-smash-friends.py', plist=PLIST),
-          ],
-          packages=['usf', 'usf.widgets', 'usf.screen', 'usf.subpixel'],
-          scripts=SCRIPTS,
-          requires=['pygame (>=1.6)', 'python (>=2.5)'],
-          data_files=(DATA + DOC + CONFIG + ICON),
-          setup_requires=['py2app'],
-          options={'py2app': OPTIONS}
-         )
+    WINDOWS = None
 else:
-    setup(name=NAME,
-          version=VERSION,
-          description=('A 2d arcade fight game, based on the gameplay of super '
-                       'smash bros, from nintendo.'),
-          author='Gabriel Pettier',
-          author_email='gabriel.pettier@gmail.com',
-          maintainer='Lucas Baudin (xapantu)',
-          maintainer_email='xapantu@gmail.com',
-          url='http://usf.tuxfamily.org/',
-          classifiers=['Development Status :: 2 - Pre-Alpha',
-                       'Operating System :: OS Independent',
-                       'Intended Audience :: End Users/Desktop',
-                       'License :: OSI Approved :: GNU General Public License (GPL)',
-                       'Natural Language :: English',
-                       'Programming Language :: Python',
-                       'Topic :: Games/Entertainment :: Arcade'
-                      ],
-          packages=['usf', 'usf.widgets', 'usf.screen', 'usf.subpixel'],
-          scripts=SCRIPTS,
-          requires=['pygame (>=1.6)', 'python (>=2.5)'],
-          data_files=(DATA + DOC + CONFIG + ICON),
-              windows=[{"script" : "ultimate-smash-friends", "icon_resources" : [(1, "data/icon/icon.ico")]}]
-         )
+    DATA = [(join('share', 'ultimate-smash-friends') + sep + item[0], item[1])
+            for item in files('data')]
+    DATA.append((join('share', 'ultimate-smash-friends') + sep + 'data', ['CREDITS.txt']))
+    WINDOWS = None
+
+setup(name=NAME, version=VERSION, description=DESCRIPTION, 
+      author=AUTHOR, author_email=AUTHOR_EMAIL, 
+      maintainer=MAINTAINER, maintainer_email=MAINTAINER_EMAIL, url=URL,
+      classsifiers=CLASSIFIERS, packages=PACKAGES, scripts=SCRIPTS, 
+      requires=REQUIRES, data_files=DATA, windows=WINDOWS)
