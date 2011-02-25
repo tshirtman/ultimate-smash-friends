54f30c10775ea7f28545c89c11a44e17e372af0b
tshirtman <gabriel.pettier@gmail.com>
2011-02-25 18:47:54 +0100

    reduced difference with outsourced loaders.py on pypi

--- usf/loaders.py
+++ usf/loaders.py
@@ -24,24 +24,20 @@
 import os, sys, copy
 import pygame
 import logging
-
-# my imports
-import loaders
-import music
-import animations
-from ConfigParser import SafeConfigParser
-
-try:
-    from pygame.locals import BLEND_MAX
-except:
-    Log().log("old version of pygame no BLEND_MAX")
-    BLEND_MAX = None
+import math
 
 from config import Config
-
 config = Config()
 
-from debug_utils import draw_rect
+try:
+    from pygame.locals import BLEND_RGBA_MAX
+    from pygame.locals import BLEND_RGBA_ADD
+    from pygame.locals import BLEND_RGBA_SUB
+    from pygame.locals import BLEND_RGBA_MULT
+    from pygame.locals import BLEND_RGBA_MIN
+except:
+    Log().log("old version of pygame no BLEND_RGBA_MAX")
+    BLEND_RGBA_MAX = None
 
 def memoize(function):
     """
@@ -80,8 +76,8 @@ def image(name, *args, **kwargs):
     if 'reversed' in kwargs and kwargs['reversed']:
         kwargs['reversed'] = False
         #logging.debug("reverse "+name)
-        image = pygame.transform.flip(
-            loaders.image(name,*args, **kwargs)[0],
+        img = pygame.transform.flip(
+            image(name,*args, **kwargs)[0],
             True, #flip horizontaly
             False #not verticaly
             )
@@ -89,18 +85,32 @@ def image(name, *args, **kwargs):
     elif 'lighten' in kwargs and kwargs['lighten']:
         #logging.debug('lightened: '+name)
         kwargs['lighten'] = False
-        image = loaders.image(name, *args, **kwargs)[0].copy()
-        if BLEND_MAX is not None:
-            image.fill(
+        img = image(name, *args, **kwargs)[0].copy()
+        if BLEND_RGBA_MAX is not None:
+            img.fill(
                     pygame.Color('lightgrey'),
                     None,
-                    BLEND_MAX
+                    BLEND_RGBA_MAX
                     )
         else:
            # this mean this version of pygame is to old to use the effect
            # above, an equivalent method would be a good thing
            logging.warning('pygame version < 1.9 no alpha blend.')
            pass #nothing for the moment 
+
+    elif 'alpha' in kwargs and kwargs['alpha'] is not None:
+        alpha = kwargs['alpha']
+        if not 0 <= alpha <= 1:
+            logging.warning('bad alpha value: %s' % alpha)
+            alpha = min(1, max(0, alpha))
+
+        kwargs['alpha'] = None
+        img = image(name, *args, **kwargs)[0].copy()
+        img.fill(
+                pygame.Color(255, 255, 255, int(alpha*255)),
+                image(name, *args, **kwargs)[1],
+                BLEND_RGBA_MULT
+                )
 
     elif 'scale' in kwargs and kwargs['scale'] is not None:
         if len(kwargs['scale']) is not 2:
@@ -110,13 +120,13 @@ def image(name, *args, **kwargs):
         scale = kwargs['scale']
         kwargs['scale'] = None
         if config.general['SMOOTHSCALE']:
-            image = pygame.transform.smoothscale(
-                loaders.image(name, *args, **kwargs)[0],
+            img = pygame.transform.smoothscale(
+                image(name, *args, **kwargs)[0],
                 scale
                 )
         else:
-            image = pygame.transform.scale(
-                loaders.image(name, *args, **kwargs)[0],
+            img = pygame.transform.scale(
+                image(name, *args, **kwargs)[0],
                 scale
                 )
 
@@ -126,35 +136,35 @@ def image(name, *args, **kwargs):
         kwargs['zoom'] = None
         #logging.debug('scaling image '+name+' :'+str(zoom))
         if config.general['SMOOTHSCALE']:
-            image = pygame.transform.smoothscale(
-                    loaders.image(name, **kwargs)[0],
+            img = pygame.transform.smoothscale(
+                    image(name, **kwargs)[0],
                     (
-                     int(loaders.image(name, *args, **kwargs)[1][2]*zoom),
-                     int(loaders.image(name, *args, **kwargs)[1][3]*zoom)
+                     int(image(name, *args, **kwargs)[1][2]*zoom),
+                     int(image(name, *args, **kwargs)[1][3]*zoom)
                     )
                     )
         else:
-            image = pygame.transform.scale(
-                    loaders.image(name, **kwargs)[0],
+            img = pygame.transform.scale(
+                    image(name, **kwargs)[0],
                     (
-                     int(loaders.image(name, *args, **kwargs)[1][2]*zoom),
-                     int(loaders.image(name, *args, **kwargs)[1][3]*zoom)
+                     int(image(name, *args, **kwargs)[1][2]*zoom),
+                     int(image(name, *args, **kwargs)[1][3]*zoom)
                     )
                     )
+
+    elif 'rotate' in kwargs and kwargs['rotate'] not in (None, 0):
+        angle = kwargs['rotate']
+        kwargs['rotate'] = None
+        img = pygame.transform.rotate(image(name, **kwargs)[0], angle *180/math.pi)
+
     else:
         try:
-            image = pygame.image.load(name)
+            img = pygame.image.load(name)
         except pygame.error, message:
             logging.debug('Cannot load image:'+str(name), 2)
             raise# SystemExit, message
-        if 'colorkey' not in kwargs or kwargs['colorkey'] is None:
-            image = image.convert_alpha()
-        if 'colorkey' in kwargs and kwargs['colorkey'] is not None:
-            image = image.convert()
-            if colorkey is -1:
-                colorkey = image.get_at((0,0))
-            image.set_colorkey(colorkey, RLEACCEL)
-    return image, image.get_rect()
+        img = img.convert_alpha()
+    return img, img.get_rect()
 
 @memoize
 def image_layer(first, second, pos=(0,0)):
@@ -205,3 +215,4 @@ def get_gconfig():
     parser.optionxform=str
     parser.read(get_config().sys_data_dir + "game.cfg")
     return parser
+
