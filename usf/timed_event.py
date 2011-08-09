b8b13a3bf333f4ed12d449caf62085e10b8b49ef
tshirtman <gabriel.pettier@gmail.com>
2011-08-09 09:42:16 +0200

    updated copyright notice

--- usf/timed_event.py
+++ usf/timed_event.py
@@ -1,21 +1,21 @@
-###############################################################################
-# copyright 2008 Gabriel Pettier <gabriel.pettier@gmail.com>
-#
-# This file is part of ultimate-smash-friends
-#
-# ultimate-smash-friends is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at your
-# option) any later version.
-#
-# ultimate-smash-friends is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# ultimate-smash-friends.  If not, see <http://www.gnu.org/licenses/>.
-###############################################################################
+################################################################################
+# copyright 2008-2011 Gabriel Pettier <gabriel.pettier@gmail.com>              #
+#                                                                              #
+# This file is part of ultimate-smash-friends                                  #
+#                                                                              #
+# ultimate-smash-friends is free software: you can redistribute it and/or      #
+# modify it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or (at your   #
+# option) any later version.                                                   #
+#                                                                              #
+# ultimate-smash-friends is distributed in the hope that it will be useful, but#
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or#
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for    #
+# more details.                                                                #
+#                                                                              #
+# You should have received a copy of the GNU General Public License along with #
+# ultimate-smash-friends.  If not, see <http://www.gnu.org/licenses/>.         #
+################################################################################
 '''
 This module allow to create timed and condition based events, to create
 complexe effects and behaviours in the game.
